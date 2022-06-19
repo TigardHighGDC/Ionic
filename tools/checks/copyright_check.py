40950829c2e0df4ac4e371423c95a040b30db562
Brandon <pacewicbrandon@gmail.com>
2022-06-19 10:00:09 -0700

    Basic test module (#32)
    * Added a copyright check
    
    * Update to `copyright_check`
    
    * Local testing tool
    
    * Added copyright check action

---
+++ tools/checks/copyright_check.py
@@ -0,0 +1,29 @@
+# Copyright (c) TigardHighGDC
+# SPDX-License-Identifier: Apache-2.0
+
+import os
+
+def main():
+    os.chdir('../')
+
+    with open('../COPYRIGHT', 'r') as file:
+        copyright = file.readlines()
+
+    for line in copyright:
+        line = f'// {line}'
+
+    # files.lst contains all files to be checked
+    with open('files.lst', 'r') as file:
+        files = file.readlines()
+
+    for file in files:
+        with open(file, 'r') as f:
+            contents = f.readlines()
+
+        if copyright[0] != contents[0] or copyright[1] != contents[1]:
+            print(f'`{file}` is missing copyright; exiting')
+            exit(1)
+
+
+if __name__ == "__main__":
+    main()
